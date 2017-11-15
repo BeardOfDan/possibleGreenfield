e54e85c4bac4cdd37d5ddc600b48d7f5b8080fee
Daniel Roberts <BeardOfDan@gmail.com>
2017-11-14 17:03:13 -0800

    add react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+* A counter button: tap the button to increase the count.
+*/
+class Counter extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Greenfield!</h1>
+      </div>
+    );
+  }
+}
+export default Counter;
